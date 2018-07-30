f14c42836448585ecf1f978d6513210a9e8911af
Chribtoper <xcstolfax@gmail.com>
2018-07-30 00:06:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(int)
 number = 2
-  if int == 0 || int < 0
+  if int < 2 
     return false
-    elsif int == 1 || int == 2 
-    return true
+  elsif int == 2
+  return true 
   end
 until number == int
 if int % number == 0 || int == 0
