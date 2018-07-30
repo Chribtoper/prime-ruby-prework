6397970d53fdc4a6855c4ed9d7c980d627f4679b
Chribtoper <xcstolfax@gmail.com>
2018-07-30 00:02:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
 def prime?(int)
+  
   if int == 0
     return false
     elsif int == 1 || int == 2 
     return true
   end
-number = 2
-abs = int.abs 
 until number == abs
 if abs % number == 0 || int == 0
   return false
